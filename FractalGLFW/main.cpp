cc5c4cbf1f12d439d84da0401ff8adaf8abba170
wladaf <wladaf@mail.ru>
2016-04-21 18:28:36 +0300

    Initial Commit

---
+++ FractalGLFW/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FractalGLFW
+//
+//  Created by Владислав Афанасьев on 21/04/16.
+//  Copyright © 2016 Владислав Афанасьев. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
